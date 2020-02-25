d79683ffd00e330199a2c0c0eeb1e1533cefd345
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-02-25 03:13:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,23 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
   def self.all
     @@all
   end
-
   def save
     self.class.all << self
   end
-
+  
+  def self.create 
+  song = self.new
+  @@all << song
+  song
+  end
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
 end
+
+ 
